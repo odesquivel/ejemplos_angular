1c31d88be857ad6b1ec7732357f718a816dd421f
guatenais <odesquivel@yahoo.com>
2021-09-16 22:28:42 -0600

    cambio de limite de consulta

--- src/app/gifs/service/gifs.service.ts
+++ src/app/gifs/service/gifs.service.ts
@@ -35,7 +35,7 @@ export class GifsService {
     }
     const params= new HttpParams()
       .set('api_key',this.apiKey)
-      .set('limit','10')
+      .set('limit','12')
       .set('q',query);
       console.log(params.toString()); 
     this.http.get<GifsResponse>(`${this.servicioUrl}/search`,{params:params})
